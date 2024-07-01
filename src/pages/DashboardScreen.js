39d341fe1894efecd7bf1dccc42d2848e2e541a0
iantanui <rutooian@gmail.com>
2024-07-01 12:27:35 +0300

    margin

--- src/pages/DashboardScreen.js
+++ src/pages/DashboardScreen.js
@@ -3,7 +3,7 @@ import DashboardCard from '../components/dashboard/DashboardCard';
 import { Container, Grid } from '@mui/material';
 
 const DashboardScreen = () => (
-  <Container>
+  <Container style={{ marginTop: '20px'}}>
     <Grid container spacing={3}>
       <Grid item xs={12} sm={6} md={4}>
         <DashboardCard title="Total Revenue" value="$0" />
