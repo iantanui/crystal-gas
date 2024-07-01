c1c83421465ce5d8483025b65bca9cc6d76c31fb
iantanui <rutooian@gmail.com>
2024-07-01 13:00:42 +0300

    inputs

--- src/components/gasTypes/GasTypesDialog.js
+++ src/components/gasTypes/GasTypesDialog.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
-import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button } from '@mui/material';
-import { useGasType } from './GasTypeContext';
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+  Button,
+} from "@mui/material";
+import { useGasType } from "./GasTypeContext";
 
 const GasTypesDialog = ({ open, onClose, gasType, isEdit }) => {
   const { addGasType, updateGasType } = useGasType();
-  const [name, setName] = useState(gasType ? gasType.name : '');
-  const [buyingPrice6kg, setBuyingPrice6kg] = useState(gasType ? gasType.buyingPrice6kg : '');
-  const [buyingPrice13kg, setBuyingPrice13kg] = useState(gasType ? gasType.buyingPrice13kg : '');
+  const [name, setName] = useState(gasType ? gasType.name : "");
+  const [buyingPrice6kg, setBuyingPrice6kg] = useState(
+    gasType ? gasType.buyingPrice6kg : ""
+  );
+  const [buyingPrice13kg, setBuyingPrice13kg] = useState(
+    gasType ? gasType.buyingPrice13kg : ""
+  );
 
   const handleSubmit = () => {
     const newGasType = { name, buyingPrice6kg, buyingPrice13kg };
@@ -20,15 +31,39 @@ const GasTypesDialog = ({ open, onClose, gasType, isEdit }) => {
 
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>{isEdit ? 'Edit Gas Type' : 'Add Gas Type'}</DialogTitle>
+      <DialogTitle>{isEdit ? "Edit Gas Type" : "Add Gas Type"}</DialogTitle>
       <DialogContent>
-        <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
-        <TextField label="6kg Buying Price" value={buyingPrice6kg} onChange={(e) => setBuyingPrice6kg(e.target.value)} fullWidth />
-        <TextField label="13kg Buying Price" value={buyingPrice13kg} onChange={(e) => setBuyingPrice13kg(e.target.value)} fullWidth />
+        <TextField
+          autoFocus
+          margin="dense"
+          type="text"
+          label="Gas Type Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          fullWidth
+        />
+        <TextField
+        autoFocus
+          margin="dense"
+          type="number"
+          label="6kg Buying Price"
+          value={buyingPrice6kg}
+          onChange={(e) => setBuyingPrice6kg(e.target.value)}
+          fullWidth
+        />
+        <TextField
+        autoFocus
+          margin="dense"
+          type="number"
+          label="13kg Buying Price"
+          value={buyingPrice13kg}
+          onChange={(e) => setBuyingPrice13kg(e.target.value)}
+          fullWidth
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={handleSubmit}>{isEdit ? 'Update' : 'Add'}</Button>
+        <Button onClick={handleSubmit}>{isEdit ? "Update" : "Add"}</Button>
       </DialogActions>
     </Dialog>
   );
