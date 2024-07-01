9bee10b331839564ba2f990343fa65b63a0cc44d
iantanui <rutooian@gmail.com>
2024-07-01 12:22:17 +0300

    Heading

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 const TopBar = () => (
   <AppBar position="static">
-    <Toolbar>
-      <Typography variant="h6">Crystal Gas Management</Typography>
+    <Toolbar style={{ backgroundColor: 'white', color: 'black', padding: '10px'}}>
+      <Box>
+        <Typography variant="h4">Crystal Gas</Typography>
+        <Typography variant="p" color={ "gray"}>Gas management made easy</Typography>
+      </Box>
     </Toolbar>
   </AppBar>
 );
