d84405ad06adbd7e0eb9f8d40d84fc1069c014c6
iantanui <rutooian@gmail.com>
2024-07-01 12:23:22 +0300

    clean

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -3,10 +3,14 @@ import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 const TopBar = () => (
   <AppBar position="static">
-    <Toolbar style={{ backgroundColor: 'white', color: 'black', padding: '10px'}}>
+    <Toolbar
+      style={{ backgroundColor: "white", color: "black", padding: "10px" }}
+    >
       <Box>
         <Typography variant="h4">Crystal Gas</Typography>
-        <Typography variant="p" color={ "gray"}>Gas management made easy</Typography>
+        <Typography variant="p" color={"gray"}>
+          Gas management made easy
+        </Typography>
       </Box>
     </Toolbar>
   </AppBar>
