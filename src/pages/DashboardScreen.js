b6e78c562ce6191e11c06809db071fc9766a4f16
iantanui <rutooian@gmail.com>
2024-07-02 20:24:03 +0300

    revenue

--- src/pages/DashboardScreen.js
+++ src/pages/DashboardScreen.js
@@ -3,7 +3,7 @@ import DashboardCard from "../components/dashboard/DashboardCard";
 import { Container, Grid, Typography } from "@mui/material";
 import { useProducts } from "../components/products/ProductContext";
 import { useRefills } from "../components/refills/RefillContext";
-import { List, Menu, Person } from "@mui/icons-material";
+import { AttachMoney, List, Menu, Person } from "@mui/icons-material";
 
 const DashboardScreen = () => {
   const { products } = useProducts();
@@ -13,6 +13,31 @@ const DashboardScreen = () => {
   const totalRefills = refills ? refills.length : 0;
   const totalCustomers = refills
     ? [...new Set(refills.map((refill) => refill.customerName))].length
+    : 0;
+
+    const grossRevenue = refills
+    ? refills.reduce((acc, refill) => {
+        const refillTotal = refill.products
+          ? refill.products.reduce((sum, product) => {
+              const productPrice = product.product.price * product.quantity;
+              return sum + productPrice;
+            }, 0)
+          : 0;
+        return acc + refillTotal;
+      }, 0)
+    : 0;
+
+  const netRevenue = refills
+    ? refills.reduce((acc, refill) => {
+        const refillTotal = refill.products
+          ? refill.products.reduce((sum, product) => {
+              const productPrice = product.product.price * product.quantity;
+              const buyingPrice = product.product.buyingPrice * product.quantity;
+              return sum + (productPrice - buyingPrice);
+            }, 0)
+          : 0;
+        return acc + refillTotal;
+      }, 0)
     : 0;
 
   return (
@@ -46,6 +71,23 @@ const DashboardScreen = () => {
             label="Total Customers"
             value={totalCustomers}
             bottom="Customers"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <DashboardCard
+            icon={<AttachMoney />}
+            label="Gross Revenue"
+            value={`KES ${grossRevenue}`}
+            bottom="Total gross revenue"
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <DashboardCard
+            icon={<AttachMoney />}
+            label="Net Revenue"
+            value={`KES ${netRevenue}`}
+            bottom="Total net revenue"
           />
         </Grid>
       </Grid>
