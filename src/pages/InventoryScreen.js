5f74fbd4580309f64cd4ab576520ef4d882b2fd7
iantanui <rutooian@gmail.com>
2024-07-09 11:47:48 +0300

    inventory table

--- src/pages/InventoryScreen.js
+++ src/pages/InventoryScreen.js
@@ -1,12 +1,39 @@
-import { Container, Typography } from "@mui/material";
 import React from "react";
+import { Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { useProducts } from "../components/products/ProductContext";
 
 const InventoryScreen = () => {
+    const { products } = useProducts();
+
   return (
-    <Container>
+    <Container style={{ marginTop: "20px" }}>
       <Typography variant="h6" color="black" gutterBottom>
         Inventory
       </Typography>
+
+      <TableContainer component={Paper}>
+      <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Gas name</TableCell>
+              <TableCell>Size</TableCell>
+              <TableCell>Quantity</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product) => (
+              <TableRow key={product.id}>
+                <TableCell>{product.name}</TableCell>
+                <TableCell>{product.size}</TableCell>
+                <TableCell>{product.quantity}</TableCell>
+                {product.quantity <= 5 && (
+                  <TableCell style={{ color: "red" }}>Low Stock!</TableCell>
+                )}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Container>
   );
 };
