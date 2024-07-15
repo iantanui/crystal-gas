7e81cc55d38f35e381df440b23da4e8227a4534c
iantanui <rutooian@gmail.com>
2024-07-15 11:48:33 +0300

    clean

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -21,11 +21,10 @@ const ProductItem = ({ index, product, onEdit, onDelete }) => {
     setAnchorEl(null);
   };
 
-   // Function to format ISO date string to DD/MM/YYYY format
-   const formatISODate = (isoDate) => {
+  const formatISODate = (isoDate) => {
     const date = new Date(isoDate);
     const day = date.getDate().toString().padStart(2, "0");
-    const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Months are zero based
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear().toString();
     return `${day}/${month}/${year}`;
   };
