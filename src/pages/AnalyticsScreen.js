ad5f702f8a47f866cf8297edbed1b4f06561d367
iantanui <rutooian@gmail.com>
2024-07-17 12:13:32 +0300

    tabs

--- src/pages/AnalyticsScreen.js
+++ src/pages/AnalyticsScreen.js
@@ -1,12 +1,23 @@
-import { Container, Typography } from "@mui/material";
-import React from "react";
+import { Container, Tab, Tabs, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 const AnalyticsScreen = () => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const handleTanChange = (event, newValue) => {
+    setTabIndex(newValue);
+  };
+
   return (
     <Container style={{ margin: "20px" }}>
       <Typography variant="h6" color="black" gutterBottom>
         Analytics
       </Typography>
+      <Tabs value={tabIndex} onChange={handleTanChange}>
+        <Tab label="Customers" />
+        <Tab value="Revenue" />
+        <Tab value="Refills" />
+      </Tabs>
     </Container>
   );
 };
