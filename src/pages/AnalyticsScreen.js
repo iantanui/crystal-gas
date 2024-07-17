6e7fb492aaf483d03b0663a6b7037d955208483b
iantanui <rutooian@gmail.com>
2024-07-17 14:47:00 +0300

    clean

--- src/pages/AnalyticsScreen.js
+++ src/pages/AnalyticsScreen.js
@@ -1,15 +1,12 @@
 import React from "react";
-import { Container, Typography} from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 const AnalyticsScreen = () => {
-  
-
   return (
     <Container>
       <Typography variant="h6" gutterBottom p={2}>
         Analytics
       </Typography>
-
     </Container>
   );
 };
