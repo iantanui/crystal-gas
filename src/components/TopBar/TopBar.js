6132d1f4a6d71f04e001185b4fd40ad3b5679e56
iantanui <rutooian@gmail.com>
2024-07-24 11:11:24 +0300

    topBar

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,19 +1,84 @@
-import React from "react";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { AccountCircleOutlined } from "@mui/icons-material";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
 
-const TopBar = () => (
-  <AppBar position="static">
-    <Toolbar
-      style={{ backgroundColor: "white", color: "black", padding: "10px" }}
-    >
-      <Box>
-        <Typography variant="h4">Crystal Gas</Typography>
-        <Typography variant="p" color={"gray"}>
-          Gas management made easy
-        </Typography>
-      </Box>
-    </Toolbar>
-  </AppBar>
-);
+const TopBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [user] = useAuthState(auth);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar
+        style={{ backgroundColor: "white", color: "black", padding: "10px" }}
+      >
+        <Box>
+          <Typography variant="h4">Crystal Gas</Typography>
+          <Typography variant="p" color={"gray"}>
+            Gas management made easy
+          </Typography>
+        </Box>
+        {user && (
+          <div>
+            <IconButton
+              edge="end"
+              aria-label="acconut of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
+            >
+              <AccountCircleOutlined />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleMenuClose}
+            >
+              <MenuItem disabled>{user.email}</MenuItem>
+              <MenuItem onClick={handleLogout}>Logout</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default TopBar;
